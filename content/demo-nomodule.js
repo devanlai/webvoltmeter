113bb5f36f86d1b6fc29e30970b83c0a18fabdbe
Devan Lai <devan.lai@gmail.com>
2019-12-01 16:26:56 -0800

    demo: detect browsers without module support

---
+++ content/demo-nomodule.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("demo-ble-check").innerHTML =
+"<p><span role=\"image\" aria-label=\"x\">❌</span>\
+Your browser does not seem to support ES6 JavaScript modules. \
+This demo is written in pure JavaScript without the use of transpilation, so it will not work in your browser. \
+Please consider switching to a <a href=\"https://caniuse.com/#feat=es6-module\">browser that has support for ES6 modules</a>\
+</p>";
+});
