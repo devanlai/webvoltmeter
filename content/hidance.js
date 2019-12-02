8aa0ed4502fd2edbcda5384ec1f05cde8f23a141
Devan Lai <devan.lai@gmail.com>
2019-12-01 21:03:59 -0800

    hidance: fix concatenation and parse data
    Parsed data is accessible via info callbacks

--- content/hidance.js
+++ content/hidance.js
@@ -11,10 +11,23 @@ let hidance_uuid128 = UUIDFactory(HiDANCE_UUID_TEMPLATE);
 const HIDANCE_SERVICE_UUID = hidance_uuid128(0xFFE0);
 const HIDANCE_SERVICE_CHAR_UUID = hidance_uuid128(0xFFE1);
 
+function getUint24(dataview, byteOffset, littleEndian=false) {
+    if (littleEndian) {
+        return ((dataview.getUint8(byteOffset)   <<  0)
+              | (dataview.getUint8(byteOffset+1) <<  8)
+              | (dataview.getUint8(byteOffset+2) << 16));
+    } else {
+        return ((dataview.getUint8(byteOffset)   << 16)
+              | (dataview.getUint8(byteOffset+1) <<  8)
+              | (dataview.getUint8(byteOffset+2) <<  0));
+    }
+}
+
 class HiDANCEEnergyMeter extends BaseBLEDevice {
     constructor(device) {
         super(device);
         this.characteristic = null;
+        this._info_callbacks = [];
         this._data_callbacks = [];
         this._packet_callbacks = [];
 
@@ -22,6 +35,19 @@ class HiDANCEEnergyMeter extends BaseBLEDevice {
         this._first_chunk_timestamp = null;
         this._second_chunk = null;
         this._second_chunk_timestamp = null;
+    }
+
+    add_info_callback(callback) {
+        if (!this._info_callbacks.includes(callback)) {
+            this._info_callbacks.push(callback);
+        }
+    }
+
+    remove_info_callback(callback) {
+        const index = this._info_callbacks.indexOf(callback);
+        if (index > -1) {
+            this._info_callbacks.splice(index, 1);
+        }
     }
 
     add_data_callback(callback) {
@@ -81,13 +107,19 @@ class HiDANCEEnergyMeter extends BaseBLEDevice {
                     ...new Uint8Array(this._first_chunk),
                     ...new Uint8Array(this._second_chunk)
                 ]);
-                let view = new DataView(data);
+                let view = new DataView(data.buffer);
                 // Dispatch callbacks for the complete data
                 for (let callback of this._data_callbacks) {
                     await callback(view);
                 }
 
-                // TODO: parse data and send parsed data via callback
+                let info = this.parse_data(view);
+                if (info) {
+                    // Dispatch callbacks for the parsed information
+                    for (let callback of this._info_callbacks) {
+                        await callback(info);
+                    }
+                }
             }
 
             this._first_chunk = null;
@@ -103,6 +135,50 @@ class HiDANCEEnergyMeter extends BaseBLEDevice {
 
     async stop_notifications() {
         await this.characteristic.stopNotifications();
+    }
+
+    parse_data(dataview) {
+        const mode = dataview.getUint8(3);
+        if (mode == 1) {
+            let info = {
+                voltage_v:          getUint24(dataview, 4) / 10.0,
+                current_a:          getUint24(dataview, 7) / 1000.0,
+                power_w:            getUint24(dataview, 10) / 10.0,
+                energy_wh:          dataview.getUint32(13) * 10.0,
+                price_per_kwh:      getUint24(dataview, 17) / 100.0,
+                frequency_hz:       dataview.getUint16(20) / 10.0,
+                power_factor:       dataview.getUint16(22) / 1000.0,
+                temp_c:             dataview.getUint16(24),
+                backlight_time_s:   dataview.getUint8(30),
+            };
+            return info;
+        } else if (mode == 2) {
+            let info = {
+                voltage_v:          getUint24(dataview, 4) / 10.0,
+                current_a:          getUint24(dataview, 7) / 1000.0,
+                charge_ah:          getUint24(dataview, 10) / 100.0,
+                energy_wh:          dataview.getUint32(13) * 100.0,
+                price_per_kwh:      getUint24(dataview, 17) / 100.0,
+                temp_c:             dataview.getUint16(24),
+                backlight_time_s:   dataview.getUint8(30),
+            };
+            return info;
+        } else if (mode == 3) {
+            let info = {
+                voltage_v:          getUint24(dataview, 4) / 100.0,
+                current_a:          getUint24(dataview, 7) / 100.0,
+                charge_ah:          getUint24(dataview, 10) / 1000.0,
+                energy_wh:          dataview.getUint32(13) / 100.0,
+                usb_dp_v:           dataview.getUint16(17) / 100.0,
+                usb_dm_v:           dataview.getUint16(19) / 100.0,
+                temp_c:             dataview.getUint16(21),
+                on_time_s:          dataview.getUint16(23),
+                backlight_time_s:   dataview.getUint8(30),
+            };
+            return info;
+        } else {
+            return null;
+        }
     }
 }
 
